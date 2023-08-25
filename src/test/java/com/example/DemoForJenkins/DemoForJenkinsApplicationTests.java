94739d1e54c32de1b07d513fa2cd43437606edc5
rk65964 <rakeshkumar961994@gmail.com>
2023-08-26 00:48:11 +0530

    first commit for jenkins job hello world

---
+++ src/test/java/com/example/DemoForJenkins/DemoForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoForJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
